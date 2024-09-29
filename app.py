fdef64bb81ea22d140b93926a193a7992d4ee007
hoangtuanlsht <151942543+hoangtuanlsht@users.noreply.github.com>
2024-09-30 00:56:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from scipy import stats
-
+import os
 # Import các mô hình từ các tệp riêng biệt
 from linear import train_and_predict_linear, evaluate_model as eval_linear
 from neural import train_and_predict_neural, evaluate_model as eval_neural
